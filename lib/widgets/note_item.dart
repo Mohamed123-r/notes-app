2a7050ce318b9becbce31fb1e3e30b3b66488bf8
Mohamed Attalla <mezoattalla123@gmail.com>
2023-11-06 00:26:17 +0200

    create note list view

---
+++ lib/widgets/note_item.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Color(0xffFFCD7B),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, top: 24, bottom: 16),
+            child: ListTile(
+              title: Text(
+                'Flutter tips',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Text(
+                  'Build your career with thew samy ',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.5),
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 27,
+                  )),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0, bottom: 16),
+            child: Text(
+              'May22 ,2023',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.7),
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
