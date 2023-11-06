27aebe9304b67f375904d2bedead1e9955a27a0c
Mohamed Attalla <mezoattalla123@gmail.com>
2023-11-07 00:45:25 +0200

    edit note view done

--- lib/widgets/edit_note_view_bode.dart
+++ lib/widgets/edit_note_view_bode.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'custom_text_field.dart';
 
 class EditNoteViewBode extends StatelessWidget {
   const EditNoteViewBode({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            CustomTextField(
+              hint: 'Title',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomTextField(
+              hint: 'Content',
+              maxLine: 5,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
