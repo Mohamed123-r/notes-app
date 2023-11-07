b906bb7266b2e5b8e9edd707e432cf19acfd6e64
Mohamed Attalla <mezoattalla123@gmail.com>
2023-11-07 13:45:33 +0200

    create note view model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,14 @@
+class NoteModel {
+  final String title;
+
+  final String sutTitle;
+  final String date;
+
+  final int color;
+
+  NoteModel(
+      {required this.title,
+      required this.sutTitle,
+      required this.date,
+      required this.color});
+}
